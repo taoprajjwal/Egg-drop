3ca805e22f7dfbb436b3effea730bb2a8a0a4e6f
taoprajjwal <taoprajjwal@gmail.com>
2016-06-17 00:21:10 +0545

    Music Button Fixed...Button Graphics and BG Later

--- Assets/Scripts/ApplicationManager.cs
+++ Assets/Scripts/ApplicationManager.cs
@@ -5,7 +5,12 @@ public class ApplicationManager : MonoBehaviour {
 	
 	public static bool hover=false;
 
-	// Begin States
+	void Awake(){
+		if (gameObject.tag != "Respawn") {
+			DontDestroyOnLoad (gameObject);
+		}
+	}
+
 	public void Quit () 
 	{
 		#if UNITY_EDITOR
@@ -25,15 +30,13 @@ public class ApplicationManager : MonoBehaviour {
 
 	public void pause(){
 		UnityEngine.SceneManagement.SceneManager.LoadScene (3, UnityEngine.SceneManagement.LoadSceneMode.Single);
-	}
+		}
 
 	public void playonresume(){
 		hover = false;
 		UnityEngine.SceneManagement.SceneManager.LoadScene (1, UnityEngine.SceneManagement.LoadSceneMode.Single);	
 
-	}
-
-
+		}
 	 
 	public void OnHover(){
 
@@ -44,7 +47,18 @@ public class ApplicationManager : MonoBehaviour {
 
 		hover = false;
 	}
+
+	public void ToggleMusic(bool musicbg){
+		AudioSource audios =gameObject.GetComponent<AudioSource> ();
+		if (musicbg == true) {
+			audios.mute = false;
+
+		}
+		else if (musicbg==false){
+			audios.mute = true;
+
+		}
+	}
+		
+
 }
-
-
-
--- Assets/Scripts/MusicToggle.cs
+++ Assets/Scripts/MusicToggle.cs
@@ -8,22 +8,29 @@ public class MusicToggle : MonoBehaviour {
 	public Sprite OnButton;
 	private bool Music=true;
 	private Button button;
+	public GameObject AppMan;
+
 
 
 	void Start () {
 		button = GetComponent<Button> ();
-
+		AppMan = GameObject.Find ("ApplicationManagers");
 	}
-	
+
 	public void MusicButtonClick(){
 		Music = !Music;
 
 			if (Music == false) {
 			button.image.sprite	= OffButton;
+			ApplicationManager Appmans=AppMan.GetComponent<ApplicationManager>();
+			Appmans.ToggleMusic (Music);
+
 		}
 			else {
 
 			button.image.sprite=OnButton;
+			ApplicationManager Appmans=AppMan.GetComponent<ApplicationManager>();
+			Appmans.ToggleMusic (true);
 			}
 
 		}
